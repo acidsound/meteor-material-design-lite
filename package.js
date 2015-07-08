7980d9f58fed9e735624de06e04931f47466d0dd
spectrum <spectrick@gmail.com>
2015-07-08 18:33:35 +0900

    add a truetype font

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'spectrum:material-design-lite',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'material-design-lite for meteor',
-  git: '',
+  git: 'https://github.com/acidsound/meteor-material-design-lite.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.addFiles(['MaterialIcons-Regular.woff2', 'material-icons.css', 'material.js', 'material.css'], 'client');
+  api.addFiles(['MaterialIcons-Regular.ttf', 'MaterialIcons-Regular.woff2', 'material-icons.css', 'material.js', 'material.css'], 'client');
 });
