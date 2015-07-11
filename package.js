a0d2dcd1a448acf3b87d613df3f3eebdfd2075f5
spectrum <spectrick@gmail.com>
2015-07-12 04:52:10 +0900

    add a wof font

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'spectrum:material-design-lite',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'material-design-lite for meteor',
   git: 'https://github.com/acidsound/meteor-material-design-lite.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.addFiles(['MaterialIcons-Regular.ttf', 'MaterialIcons-Regular.woff2', 'material-icons.css', 'material.js', 'material.css'], 'client');
+  api.addFiles(['MaterialIcons-Regular.ttf', 'MaterialIcons-Regular.woff', 'MaterialIcons-Regular.woff2', 'material-icons.css', 'material.js', 'material.css'], 'client');
 });
